fdb63ca322e531c7f70d4127367bb3416f801ee4
hellov1ncent <113193203+hellov1ncent@users.noreply.github.com>
2023-10-06 10:14:53 -0400

    dsd

---
+++ app.js
@@ -0,0 +1,30 @@
+const students = [
+    {
+        firstName: "Vincent",
+        lastname: "Lin",
+        graduated: false,
+        DOB: 2008,
+        siblings: ["Ming","Harvey"],
+    },
+    {
+        firstName: "Hao",
+        lastname: "Ming",
+        graduated: false,
+        DOB: 2008,
+        siblings: ["Benson","Max"],
+    },
+    {
+        firstName: "Henry",
+        lastname: "idk",
+        graduated: false,
+        DOB: 2006,
+        siblings: ["Bonsy","Danny"],
+    },
+];
+//array stores irems and can access items by their index starting at 0
+
+console.log(students[students.firstName]);
+//foreach to run function on every item (for loop)
+students.forEach((students)=> console.log(students.firstName))
+const graduates = students.filter((students)=> students.graduated)
+console.log
